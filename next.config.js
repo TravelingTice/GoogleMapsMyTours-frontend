42b466ddff367154b09094327056a67f2938e86f
TravelingTice <matthijs29@live.com>
2020-05-08 15:23:48 +0200

    Add next.config.js file

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const withPWA = require('next-pwa');
+const cacheOptions = require('./cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV !== 'production',
+    register: process.env.NODE_ENV === 'production',
+    runtimeCaching: cacheOptions
+  },
+  publicRuntimeConfig: {
+    APP_NAME: 'Google Maps My Tours',
+    PRODUCTION: process.env.NODE_ENV === 'production',
+    DOMAIN_DEV: 'http://localhost:3000',
+    DOMAIN_PROD: 'https://google-maps-my-tours.now.sh',
+    API_DEV: 'http://localhost:3001/v1',
+    API_PROD: 'https://google-maps-my-tours-api.herokuapp.com/v1'
+  }
+})

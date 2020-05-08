23606c11535c24898a6ab39590ff6107f5a8ceb4
TravelingTice <matthijs29@live.com>
2020-05-08 15:10:55 +0200

    Add theme

---
+++ theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1E88E5',
+    },
+    secondary: {
+      main: '#333'
+    }
+  }
+});
+
+export default theme;

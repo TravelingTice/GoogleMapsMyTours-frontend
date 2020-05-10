e59626468985a39e87b5f22b39d23fa931465f6c
TravelingTice <matthijs29@live.com>
2020-05-10 16:41:13 +0200

    Install google map react

--- pages/maps/new.js
+++ pages/maps/new.js
@@ -1,14 +1,17 @@
 import Private from '../../components/auth/Private';
 import Layout from '../../components/layout/Layout';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 const MapsNew = () => {
   return (
     <Private>
       <Layout>
-        
+
       </Layout>
     </Private>
   )
 }
 
-export default MapsNew
+export default GoogleApiWrapper({
+  apiKey: ''
+})(MapsNew)

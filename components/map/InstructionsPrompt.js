62932b462b43ee4cdd930d3eb05ec41a4d8f2fbf
TravelingTice <matthijs29@live.com>
2020-05-12 14:05:25 +0200

    Make prompt look better

--- components/map/InstructionsPrompt.js
+++ components/map/InstructionsPrompt.js
@@ -12,8 +12,8 @@ const InstructionsPrompt = () => {
   const text = texts[state] || '';
 
   return text && selectedMarkerIcon ? (
-    <div className="mt-5">
-      <p>{text}</p>
+    <div style={{position: 'absolute', zIndex: 2, top: 65, width: '100%'}} className="d-flex justify-content-center">
+      <span className="p-2 text-white" style={{margin: '0 auto', borderRadius: 5, backgroundColor: 'rgba(0,0,0,.5)'}}>{text}</span>
     </div>
   ) : null;
 }

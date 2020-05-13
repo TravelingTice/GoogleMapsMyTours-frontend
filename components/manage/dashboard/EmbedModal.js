d0a992fedf08a39363a993e15f64168f486577f0
TravelingTice <matthijs29@live.com>
2020-05-13 13:21:36 +0200

    Add render code for

--- components/manage/dashboard/EmbedModal.js
+++ components/manage/dashboard/EmbedModal.js
@@ -2,13 +2,16 @@ import { useContext } from "react";
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { DashboardContext } from "../../../contexts/DashboardContext";
 import ReactMarkdown from 'react-markdown';
+import renderCodeFor from '../../../helpers/renderCodeFor';
 
 const EmbedModal = () => {
   const { isEmbedModal, setEmbedModal, selectedMap } = useContext(DashboardContext);
 
   const closeModal = () => setEmbedModal(false);
 
-  const input = `\`Yo\``
+  const input = `\`
+    ${renderCodeFor(selectedMap)}
+  \``
   
   return (
     <Modal isOpen={isEmbedModal} toggle={closeModal}>

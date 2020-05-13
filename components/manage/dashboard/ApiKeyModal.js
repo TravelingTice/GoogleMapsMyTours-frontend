eb70d394f7b8afff85957189af2f5d88adec6b80
TravelingTice <matthijs29@live.com>
2020-05-13 15:17:32 +0200

    Change styles of origins form

--- components/manage/dashboard/ApiKeyModal.js
+++ components/manage/dashboard/ApiKeyModal.js
@@ -1,11 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { Modal, ModalHeader, ModalBody, Form } from 'reactstrap';
-import { FormGroup, FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import { FormGroup, FormControl, InputLabel, Input, Button, IconButton } from '@material-ui/core';
 import Error from '../../Error';
 import { DashboardContext } from "../../../contexts/DashboardContext";
 import { getCookie, isAuth } from '../../../actions/auth';
 import { getApiKey, addApiKey, addOrigin } from '../../../actions/apikey';
 import generateId from 'generate-unique-id';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const ApiKeyModal = () => {
   const { isApiKeyModal, setApiKeyModal, apiKeyModalError, setApiKeyModalError } = useContext(DashboardContext);
@@ -48,19 +49,26 @@ const ApiKeyModal = () => {
     // check the new origin
     if (origin.match(/https?/)) return setApiKeyModalError('Don\'t include http or https protocol');
 
-    // add origin
+    // add origin to the api key
+    setApiKey({ ...apiKey, origins: apiKey.origins.concat({ address: origin }) });
+
     const data = await addOrigin(origin, apiKey.id, token);
+
+    if (data.error) setApiKeyModalError(data.error);
   }
 
   const listOrigins = () => apiKey.origins.map(origin => (
-    <p className="p-2 mr-2" style={{backgroundColor: '#bbb'}}></p>
+    <div className="d-flex justify-content-between">
+      <span className="p-2 mr-2 mb-2" style={{backgroundColor: '#ddd', borderRadius: 5 }}>{origin.address}</span>
+      <IconButton color="primary"><DeleteIcon/></IconButton>
+    </div>
   ))
 
   const showOriginsForm = () => (
     <Form onSubmit={handleSubmit}>
       <p>Origins:</p>
       {listOrigins()}
-      <FormGroup>
+      <FormGroup className="mt-3">
         <FormControl>
           <InputLabel htmlFor="origins">Add origin</InputLabel>
           <Input value={origin} onChange={handleChange} />
@@ -80,9 +88,9 @@ const ApiKeyModal = () => {
   
   return (
     <Modal isOpen={isApiKeyModal} toggle={closeModal}>
-      <ModalHeader toggle={closeModal}>Manage my Api Keys</ModalHeader>
+      <ModalHeader toggle={closeModal}>Manage my Api Key</ModalHeader>
 
-      <ModalBody style={{overflow: 'scroll'}}>
+      <ModalBody style={{overflow: 'scroll', paddingBottom: 50}}>
         {showError()}
 
         {!apiKey ? showButton() : (

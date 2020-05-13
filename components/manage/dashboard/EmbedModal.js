7700c1df6dbbc6807b948695cc2ffb1185efe75d
TravelingTice <matthijs29@live.com>
2020-05-13 13:14:24 +0200

    Add markdown package

--- components/manage/dashboard/EmbedModal.js
+++ components/manage/dashboard/EmbedModal.js
@@ -1,18 +1,21 @@
 import { useContext } from "react";
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { DashboardContext } from "../../../contexts/DashboardContext";
+import ReactMarkdown from 'react-markdown';
 
 const EmbedModal = () => {
   const { isEmbedModal, setEmbedModal, selectedMap } = useContext(DashboardContext);
 
   const closeModal = () => setEmbedModal(false);
+
+  const input = `\`Yo\``
   
   return (
     <Modal isOpen={isEmbedModal} toggle={closeModal}>
       <ModalHeader toggle={closeModal}>Embed map {selectedMap.name}</ModalHeader>
 
       <ModalBody>
-
+        <ReactMarkdown source={input} />
       </ModalBody>
     </Modal>
   )
